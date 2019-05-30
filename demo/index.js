04b19ca87ca56f28bef4d7670dd0b2b4f18465b1
wangjian4 <wangjian4@yy.com>
2019-05-30 10:13:33 +0800

    添加理解注释，修改demo辅助测试

--- demo/index.js
+++ demo/index.js
@@ -1,124 +1,203 @@
-import { createElement, render, Component, Fragment } from 'preact';
+import {
+  createElement,
+  render,
+  hydrate,
+  Component,
+  options,
+  Fragment
+} from '../src/index'
+import { useEffect, useState } from '../hooks/src/index'
 // import renderToString from 'preact-render-to-string';
-import './style.scss';
-import { Router, Link } from 'preact-router';
-import Pythagoras from './pythagoras';
-import Spiral from './spiral';
-import Reorder from './reorder';
-import Todo from './todo';
-import Fragments from './fragments';
-import Context from './context';
-import installLogger from './logger';
-import ProfilerDemo from './profiler';
-import KeyBug from './key_bug';
-import StateOrderBug from './stateOrderBug';
-import PeopleBrowser from './people';
-import StyledComp from './styled-components';
-import { initDevTools } from 'preact/debug/src/devtools';
-import { initDebug } from 'preact/debug/src/debug';
-import DevtoolsDemo from './devtools';
-import SuspenseDemo from './suspense';
+import './style.scss'
+import { Router, Link } from 'preact-router'
+import Pythagoras from './pythagoras'
+import Spiral from './spiral'
+import Reorder from './reorder'
+import Todo from './todo'
+import Fragments from './fragments'
+import Context from './context'
+import installLogger from './logger'
+import ProfilerDemo from './profiler'
+import KeyBug from './key_bug'
+import PeopleBrowser from './people'
+import { initDevTools } from 'preact/debug/src/devtools'
+import { initDebug } from 'preact/debug/src/debug'
+import DevtoolsDemo from './devtools'
 
-let isBenchmark = /(\/spiral|\/pythagoras|[#&]bench)/g.test(window.location.href);
+let isBenchmark = /(\/spiral|\/pythagoras|[#&]bench)/g.test(
+  window.location.href
+)
 if (!isBenchmark) {
-	// eslint-disable-next-line no-console
-	console.log('Enabling devtools and debug');
-	initDevTools();
-	initDebug();
+  // eslint-disable-next-line no-console
+  console.log('Enabling devtools and debug')
+  initDevTools()
+  initDebug()
 }
 
 // mobx-state-tree fix
-window.setImmediate = setTimeout;
+window.setImmediate = setTimeout
 
-class Home extends Component {
-	render() {
-		return (
-			<div>
-				<h1>Hello</h1>
-			</div>
-		);
-	}
+// class Home extends Component {
+//   a = 1;
+//   state = {
+//   	name: 'world',
+//     type: 1
+//   }
+//   setRandom = () => {
+//     this.setState({
+//       name: 'world' + ~~(Math.random() * 10)
+//     })
+//   }
+//   setType() {
+//     this.setState({
+//       type: Math.round(Math.random())
+//     })
+//   }
+// 	render() {
+//     const Cpn = this.state.type === 1 ? () => <div className="type1"><span>type1</span></div> : () => <span>type2</span>
+// 		return (
+// 			<div>
+// 				<h1>Hello, {this.state.name}</h1>
+//         <button onClick={this.setRandom}>random</button>
+//         <button onClick={() => this.setType()}>setType</button>
+//         <h2>end</h2>
+//         <Cpn></Cpn>
+// 			</div>
+// 		);
+// 	}
+// }
+
+const h1 = () => <h1>1111</h1>
+
+// class Home extends Component {
+//   state = {
+//     type: 1
+//   }
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({
+//         type: 2
+//       })
+//     }, 3000)
+//   }
+//   render() {
+//     const Back =
+//       this.state.type === 2 ? () => <h2>2222</h2> : h1
+//     return (
+//       <div>
+//         <h1 />
+//         <Back />
+//       </div>
+//     )
+//   }
+// }
+
+const Home = () => {
+  const [state, setState] = useState(0)
+  useEffect(() => {
+    console.log('effect active!')
+  }, [state])
+  const random = () => {
+    setState(parseInt(Math.random() * 10))
+  }
+  return (
+    <div onClick={random}>{state}</div>
+  )
 }
 
 class DevtoolsWarning extends Component {
-	onClick = () => {
-		window.location.reload();
-	}
+  onClick = () => {
+    window.location.reload()
+  }
 
-	render() {
-		return (
-			<button onClick={this.onClick}>Start Benchmark (disables devtools)</button>
-		);
-	}
+  render() {
+    return (
+      <button onClick={this.onClick}>
+        Start Benchmark (disables devtools)
+      </button>
+    )
+  }
 }
 
 class App extends Component {
-	render({ url }) {
-		return (
-			<div class="app">
-				<header>
-					<nav>
-						<Link href="/" activeClassName="active">Home</Link>
-						<Link href="/reorder" activeClassName="active">Reorder</Link>
-						<Link href="/spiral" activeClassName="active">Spiral</Link>
-						<Link href="/pythagoras" activeClassName="active">Pythagoras</Link>
-						<Link href="/todo" activeClassName="active">ToDo</Link>
-						<Link href="/fragments" activeClassName="active">Fragments</Link>
-						<Link href="/key_bug" activeClassName="active">Key Bug</Link>
-						<Link href="/profiler" activeClassName="active">Profiler</Link>
-						<Link href="/context" activeClassName="active">Context</Link>
-						<Link href="/devtools" activeClassName="active">Devtools</Link>
-						<Link href="/empty-fragment" activeClassName="active">Empty Fragment</Link>
-						<Link href="/people" activeClassName="active">People Browser</Link>
-						<Link href="/state-order" activeClassName="active">State Order</Link>
-						<Link href="/styled-components" activeClassName="active">Styled Components</Link>
-						<Link href="/suspense" activeClassName="active">Suspense / lazy</Link>
-					</nav>
-				</header>
-				<main>
-					<Router url={url}>
-						<Home path="/" />
-						<StateOrderBug path="/state-order" />
-						<Reorder path="/reorder" />
-						<div path="/spiral">
-							{!isBenchmark
-								? <DevtoolsWarning />
-								: <Spiral />
-							}
-						</div>
-						<div path="/pythagoras">
-							{!isBenchmark
-								? <DevtoolsWarning />
-								: <Pythagoras />
-							}
-						</div>
-						<Todo path="/todo" />
-						<Fragments path="/fragments" />
-						<ProfilerDemo path="/profiler" />
-						<KeyBug path="/key_bug" />
-						<Context path="/context" />
-						<DevtoolsDemo path="/devtools" />
-						<SuspenseDemo path="/suspense" />
-						<EmptyFragment path="/empty-fragment" />
-						<PeopleBrowser path="/people/:user?" />
-						<StyledComp path="/styled-components" />
-					</Router>
-				</main>
-			</div>
-		);
-	}
+  render({ url }) {
+    return (
+      <div class="app">
+        <header>
+          <nav>
+            <Link href="/" activeClassName="active">
+              Home
+            </Link>
+            <Link href="/reorder" activeClassName="active">
+              Reorder
+            </Link>
+            <Link href="/spiral" activeClassName="active">
+              Spiral
+            </Link>
+            <Link href="/pythagoras" activeClassName="active">
+              Pythagoras
+            </Link>
+            <Link href="/todo" activeClassName="active">
+              ToDo
+            </Link>
+            <Link href="/fragments" activeClassName="active">
+              Fragments
+            </Link>
+            <Link href="/key_bug" activeClassName="active">
+              Key Bug
+            </Link>
+            <Link href="/profiler" activeClassName="active">
+              Profiler
+            </Link>
+            <Link href="/context" activeClassName="active">
+              Context
+            </Link>
+            <Link href="/devtools" activeClassName="active">
+              Devtools
+            </Link>
+            <Link href="/empty-fragment" activeClassName="active">
+              Empty Fragment
+            </Link>
+            <Link href="/people" activeClassName="active">
+              People Browser
+            </Link>
+          </nav>
+        </header>
+        <main>
+          <Router url={url}>
+            <Home path="/" />
+            <Reorder path="/reorder" />
+            <div path="/spiral">
+              {!isBenchmark ? <DevtoolsWarning /> : <Spiral />}
+            </div>
+            <div path="/pythagoras">
+              {!isBenchmark ? <DevtoolsWarning /> : <Pythagoras />}
+            </div>
+            <Todo path="/todo" />
+            <Fragments path="/fragments" />
+            <ProfilerDemo path="/profiler" />
+            <KeyBug path="/key_bug" />
+            <Context path="/context" />
+            <DevtoolsDemo path="/devtools" />
+            <EmptyFragment path="/empty-fragment" />
+            <PeopleBrowser path="/people/:user?" />
+          </Router>
+        </main>
+      </div>
+    )
+  }
 }
 
 function EmptyFragment() {
-	return <Fragment />;
+  return <Fragment />
 }
 
 // document.body.innerHTML = renderToString(<App url={location.href.match(/[#&]ssr/) ? undefined : '/'} />);
 // document.body.firstChild.setAttribute('is-ssr', 'true');
 
 installLogger(
-	String(localStorage.LOG)==='true' || location.href.match(/logger/),
-	String(localStorage.CONSOLE)==='true' || location.href.match(/console/)
-);
+  String(localStorage.LOG) === 'true' || location.href.match(/logger/),
+  String(localStorage.CONSOLE) === 'true' || location.href.match(/console/)
+)
 
-render(<App />, document.body);
+render(<Home />, document.body)
--- src/render.js
+++ src/render.js
@@ -1,39 +1,41 @@
-import { EMPTY_OBJ, EMPTY_ARR } from './constants';
-import { commitRoot } from './diff/index';
-import { diffChildren } from './diff/children';
-import { createElement, Fragment } from './create-element';
-import options from './options';
+import { EMPTY_OBJ, EMPTY_ARR } from './constants'
+import { commitRoot } from './diff/index'
+import { diffChildren } from './diff/children'
+import { createElement, Fragment } from './create-element'
+import options from './options'
 
 /**
  * Render a Preact virtual node into a DOM element
  * @param {import('./index').ComponentChild} vnode The virtual node to render
  * @param {import('./internal').PreactElement} parentDom The DOM element to
  * render into
- * @param {Element | Text} [replaceNode] Attempt to re-use an
- * existing DOM tree rooted at `replaceNode`
  */
-export function render(vnode, parentDom, replaceNode) {
-	if (options.root) options.root(vnode, parentDom);
-	let oldVNode = parentDom._prevVNode;
-	vnode = createElement(Fragment, null, [vnode]);
-
-	let mounts = [];
-	diffChildren(
-		parentDom,
-		replaceNode ? vnode : (parentDom._prevVNode = vnode),
-		oldVNode,
-		EMPTY_OBJ,
-		parentDom.ownerSVGElement !== undefined,
-		replaceNode
-			? [replaceNode]
-			: oldVNode
-				? null
-				: EMPTY_ARR.slice.call(parentDom.childNodes),
-		mounts,
-		vnode,
-		replaceNode || EMPTY_OBJ
-	);
-	commitRoot(mounts, vnode);
+export function render(vnode, parentDom) {
+  // debug模式, 在debug模式下会对vnode的render diff等函数做切面以进行错误检测
+  if (options.root) options.root(vnode, parentDom)
+  // 获取前一个 vnode？ 目测在dom中会设置当前vnode和前一个vnode
+  let oldVNode = parentDom._prevVNode
+  // 创建新的vnode，Fragment是一个空函数， 相当于工厂函数，cildren是 vnode组成的数组
+  vnode = createElement(Fragment, null, [vnode])
+  let mounts = []
+  // 对比dom的children， 首先将 生成的vnode设置给 dom的_prevVnode，
+  // 然后进行对比
+  // ctx = empty_obj
+  // isSvg 通过 ownerSVGElement判断
+  // excessElement，代表旧vnode的子vnode
+  // 深度遍历(后置)children将component存到mounts中
+  diffChildren(
+    parentDom,
+    (parentDom._prevVNode = vnode),
+    oldVNode,
+    EMPTY_OBJ,
+    parentDom.ownerSVGElement !== undefined,
+    oldVNode ? null : EMPTY_ARR.slice.call(parentDom.childNodes),
+    mounts,
+    vnode
+  )
+  // 通知完成即 componentDidMount，由此可知其顺序为由里到外，顺序执行
+  commitRoot(mounts, vnode)
 }
 
 /**
@@ -43,6 +45,6 @@ export function render(vnode, parentDom, replaceNode) {
  * update
  */
 export function hydrate(vnode, parentDom) {
-	parentDom._prevVNode = null;
-	render(vnode, parentDom);
+  parentDom._prevVNode = null
+  render(vnode, parentDom)
 }
